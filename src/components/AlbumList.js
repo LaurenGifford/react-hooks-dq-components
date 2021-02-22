42eec4f9dd4849f8d05e58d06f826a978c235653
LaurenGifford <lauren.gifford.17@gmail.com>
2021-02-22 12:08:33 -0500

    complete

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Album from "./Album";
+import Album from "./AlbumCard";
 import { albums } from "../data/albums";
 
 function AlbumList() {
+  const albumCards = albums.map(album => <Album 
+  name={album.name} 
+  image={album.image} 
+  genre={album.genre}
+  />)
   return (
     <section className="albums">
-      <Album
+      {albumCards}
+      {/* <Album
         name="Classic Tuna Cans Being Opened"
         image="https://target.scene7.com/is/image/Target/GUEST_360eeed2-1747-4b15-8394-a22c7cd12230?wid=488&hei=488&fmt=pjpeg"
         genre="Cats"
@@ -24,7 +30,7 @@ function AlbumList() {
         name="Aloha K.K."
         image="https://vignette.wikia.nocookie.net/animalcrossing/images/5/5d/NH-Album_Cover-Aloha_K.K..png/revision/latest?cb=20200502231742"
         genre="Dogs"
-      />
+      /> */}
     </section>
   );
 }
